01ec5e8ab80391e5a170f71f37b4478ef8c703f0
Ivan Foschini <ivan@dc.ufscar.br>
2018-10-04 17:13:06 -0300

    Refatoração em agencia.

--- src/main/java/br/ufscar/dc/latosensu/aplicacaofinanceira/service/AgenciaServiceImpl.java
+++ src/main/java/br/ufscar/dc/latosensu/aplicacaofinanceira/service/AgenciaServiceImpl.java
@@ -6,16 +6,11 @@ import br.ufscar.dc.latosensu.aplicacaofinanceira.exception.ValidationException;
 import br.ufscar.dc.latosensu.aplicacaofinanceira.model.Agencia;
 import br.ufscar.dc.latosensu.aplicacaofinanceira.model.Banco;
 import br.ufscar.dc.latosensu.aplicacaofinanceira.model.Cidade;
-import br.ufscar.dc.latosensu.aplicacaofinanceira.model.Endereco;
 import br.ufscar.dc.latosensu.aplicacaofinanceira.repository.AgenciaRepository;
 import br.ufscar.dc.latosensu.aplicacaofinanceira.repository.BancoRepository;
 import br.ufscar.dc.latosensu.aplicacaofinanceira.repository.CidadeRepository;
 import br.ufscar.dc.latosensu.aplicacaofinanceira.validation.ValidationUtil;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
-import javax.validation.ConstraintViolation;
-import javax.validation.Validation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.data.domain.Sort;
@@ -23,7 +18,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 
 @Service
 @Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
@@ -72,7 +66,7 @@ public class AgenciaServiceImpl implements AgenciaService {
     @Override
     @Transactional(propagation = Propagation.REQUIRED, readOnly = false)
     public Agencia save(Agencia agencia, BindingResult bindingResult) throws NotFoundException, NotUniqueException, ValidationException {
-        validateAgenciaAndEndereco(agencia, bindingResult);
+        new ValidationUtil().validateAgenciaAndEndereco(agencia, bindingResult);
         validateBanco(agencia);
         validateCidade(agencia);        
         
@@ -86,7 +80,7 @@ public class AgenciaServiceImpl implements AgenciaService {
     @Override
     @Transactional(propagation = Propagation.REQUIRED, readOnly = false)
     public Agencia update(long id, Agencia agencia, BindingResult bindingResult) throws NotFoundException, NotUniqueException, ValidationException {
-        validateAgenciaAndEndereco(agencia, bindingResult);   
+        new ValidationUtil().validateAgenciaAndEndereco(agencia, bindingResult);   
         validateBanco(agencia);
         validateCidade(agencia);
         
@@ -125,19 +119,6 @@ public class AgenciaServiceImpl implements AgenciaService {
         return agencia == null ? true : false;
     }
     
-    private void validateAgenciaAndEndereco(Agencia agencia, BindingResult bindingResult) throws ValidationException {
-        List<FieldError> enderecoFieldErrors = validateEndereco(agencia.getEndereco());
-        
-        if (bindingResult.hasErrors() || !enderecoFieldErrors.isEmpty()) {
-            List<FieldError> fieldErrors = new ArrayList<>(); 
-            
-            fieldErrors.addAll(bindingResult.getFieldErrors());
-            fieldErrors.addAll(enderecoFieldErrors);
-            
-            new ValidationUtil().validate(fieldErrors);
-        }
-    }
-
     private void validateBanco(Agencia agencia) throws NotFoundException {
         Banco banco = bancoRepository.findById(agencia.getBanco().getId().longValue());
         
@@ -152,19 +133,5 @@ public class AgenciaServiceImpl implements AgenciaService {
         if (cidade == null) {
             throw new NotFoundException(messageSource.getMessage("cidadeNaoEncontrada", null, null));
         }
-    }
-    
-    private List<FieldError> validateEndereco(Endereco endereco) {
-        Set<ConstraintViolation<Endereco>> enderecoViolations = Validation.buildDefaultValidatorFactory().getValidator().validate(endereco);
-        
-        List<FieldError> fieldErrors = new ArrayList<>();  
-        
-        for (ConstraintViolation<Endereco> violation: enderecoViolations) {
-            FieldError enderecoFieldError = new FieldError("endereco", "field", violation.getMessage());
-            
-            fieldErrors.add(enderecoFieldError);
-        }
-        
-        return fieldErrors;
     }
 }
