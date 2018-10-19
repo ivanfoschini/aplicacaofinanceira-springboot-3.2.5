12141f483319198d37ce60991a5de74bab71e58e
Ivan Foschini <ivan@dc.ufscar.br>
2018-10-19 14:09:04 -0300

    Implementação da parte referente à segurança.

--- src/main/java/br/ufscar/dc/latosensu/aplicacaofinanceira/AplicacaoFinanceiraApplicationConfig.java
+++ src/main/java/br/ufscar/dc/latosensu/aplicacaofinanceira/AplicacaoFinanceiraApplicationConfig.java
@@ -5,16 +5,27 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.util.AntPathMatcher;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class AplicacaoFinanceiraApplicationConfig extends WebMvcConfigurerAdapter {
+public class AplicacaoFinanceiraApplicationConfig implements WebMvcConfigurer {
 
     @Autowired
     AutorizacaoInterceptor autorizacaoInterceptor;
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(autorizacaoInterceptor).excludePathPatterns("/login").pathMatcher(new AntPathMatcher());
+        registry
+                .addInterceptor(autorizacaoInterceptor)
+                .addPathPatterns("/agencia/*")
+                .addPathPatterns("/banco/*")
+                .addPathPatterns("/cidade/*")
+                .addPathPatterns("/clientePessoaFisica/*")
+                .addPathPatterns("/clientePessoaJuridica/*")
+                .addPathPatterns("/contaCorrente/*")
+                .addPathPatterns("/contaPoupanca/*")
+                .addPathPatterns("/correntista/*")
+                .addPathPatterns("/estado/*")
+                .pathMatcher(new AntPathMatcher());
     }    
 }
