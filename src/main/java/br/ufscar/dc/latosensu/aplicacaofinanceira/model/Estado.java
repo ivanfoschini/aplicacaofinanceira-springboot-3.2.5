08d36a2ab0727b198b1509afbce169774cc93ee6
Ivan Foschini <ivanfoschini@tokenlab.com.br>
2022-12-19 16:26:20 -0300

    Estado

--- src/main/java/br/ufscar/dc/latosensu/aplicacaofinanceira/model/Estado.java
+++ src/main/java/br/ufscar/dc/latosensu/aplicacaofinanceira/model/Estado.java
@@ -1,14 +1,14 @@
 package br.ufscar.dc.latosensu.aplicacaofinanceira.model;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.SequenceGenerator;
-import javax.persistence.Table;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 @Entity
 @Table(name = "estado")
